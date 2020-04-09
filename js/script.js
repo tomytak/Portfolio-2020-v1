f510b6f718cc06c32e024f976f29205f06f2219c
tomytak <tomyt@hawaii.edu>
2020-04-09 13:37:49 -1000

    started js revealer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
-document.addEventListener("DOMContentLoaded", function() {
-  //JS
+document.addEventListener("DOMContentLoaded", function () {
+
+
+
+
+
+
+
+  // reveal point from bottom and top of the window
+  var revealerpoint = 150;
+  window.addEventListener('scroll', reveal);
+  reveal();
+
+  function reveal() {
+    console.log("scrolling");
+    var revealers = document.querySelectorAll('.revealer');
+    for (var i = 0; i < revealers.length; i++) {
+      var windowheight = window.innerHeight;
+      var revealertop = revealers[i].getBoundingClientRect().top;
+      var revealerbottom = revealers[i].getBoundingClientRect().bottom;
+      //console.log("revealertop: " + revealertop);
+      //console.log("revealerbottom: " + revealerbottom);
+      if (revealertop < windowheight - revealerpoint) {
+        revealers[i].classList.add('active')
+      } else {
+        revealers[i].classList.remove('active');
+      };
+      if (revealerbottom < 0 + revealerpoint) {
+        revealers[i].classList.remove('active');
+      }
+    }
+  };
+
+
+
+
+
+
 });
+
+
+
+
